57897294e72eb315c174f5dff50838e43da37dbc
Hamid <Goldenstarc@gmail.com>
2020-05-11 01:16:27 +0430

    INIT

---
+++ run.sh
@@ -0,0 +1,2 @@
+python3 detect.py --cfg yolov3-tiny-7cls.cfg --weights weights/last_ringAll.pt --name data/jewelry.names --iou-thres 0.1 --conf-thres 0.4
+python3 detect.py --cfg yolov3-tiny-7cls.cfg --weights weights/last_ringAll.pt --name data/jewelry.names --iou-thres 0.1 --conf-thres 0.6 --source 0 
